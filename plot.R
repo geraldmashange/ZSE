083fdec1ca73a01d10959ebb83dededd27ab98bc
geraldmashange <geraldmashange@icoud.com>
2020-08-07 20:27:09 -0500

    New R file

---
+++ plot.R
@@ -0,0 +1,5 @@
+library(ggplot2)
+ggplot(diamonds)  # if only the dataset is known.
+ggplot(diamonds, aes(x=carat))  # if only X-axis is known. The Y-axis can be specified in respective geoms.
+ggplot(diamonds, aes(x=carat, y=price))  # if both X and Y axes are fixed for all layers.
+ggplot(diamonds, aes(x=carat, color=cut))  # Each category of the 'cut' variable will now have a distinct  color, once a geom is added.
